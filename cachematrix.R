fec13081776cdeeaa5648a459f3305da18203084
stewartdesoto <stewart.desoto@yahoo.com>
2014-05-23 15:29:52 -0500

    got it working

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  #if an object is called without a method
+  m <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() return(x)
+  
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() return(m)
+  
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
